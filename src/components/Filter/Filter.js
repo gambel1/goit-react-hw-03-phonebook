bf94629f3d9f4b054b4bd067281e3e30d64be0e7
Алексей <electrifyx@gmail.com>
2023-04-17 12:39:23 +0300

    get npm instal

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,22 @@
+import { FilterWrapper, FilterLabel, FilterInput } from './Filter.styled';
+import PropTypes from 'prop-types';
+
+export default function Filter(props) {
+  const { filterValue, filterContact } = props;
+  return (
+    <FilterWrapper>
+      <FilterLabel htmlFor="filter">Find contacts by name</FilterLabel>
+      <FilterInput
+        name="filter"
+        type="text"
+        id="filter"
+        value={filterValue}
+        onChange={filterContact}
+      />
+    </FilterWrapper>
+  );
+}
+Filter.propTypes = {
+  filterValue: PropTypes.string.isRequired,
+  filterContact: PropTypes.func.isRequired,
+};
